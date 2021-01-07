7e72c828dc24ff76d4c31634f8705d3ff63d478a
cthyuii <74233671+cthyuii@users.noreply.github.com>
2021-01-07 18:37:41 +0800

    Added search bar functionality onclick abilites etc.

--- Script.js
+++ Script.js
@@ -1,5 +1,5 @@
 const pokedex = document.getElementById('pokedex');
-const cache ={};
+
 function fetchPokemon() {
 	const promises = [];
 	for (let i = 1; i <= 898; i++) {
@@ -32,7 +32,7 @@ const displayPokemon = (pokemon) => {
             (pokeman) => `
         <li class="card" onclick="pokemoninfo(${pokeman.id})">
             <img class="card-image" src="${pokeman.image}"/>
-            <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+            <a href="#" style="color: inherit;text-decoration: none;"><h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2></a>
             <p class="card-subtitle">Type: ${caps(pokeman.type)}</p>
         </li>
     `
@@ -42,14 +42,14 @@ const displayPokemon = (pokemon) => {
 };
 
 const pokemoninfo = async (id) =>{
-	if(!cache[id]){
+	
 	const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
 	const res = await fetch(url);
 	const pokeman = await res.json();
-	cache[id] = pokeman;
+
 	DisplayPopup(pokeman);
-	}
-	DisplayPopup(cache[id]);
+	
+	
 };
 
 const DisplayPopup = (pokeman) =>{
@@ -65,7 +65,8 @@ const DisplayPopup = (pokeman) =>{
 				<p><small>Abilities: </small>${caps(ability)} |
 				<small>Height(m): </small>${(pokeman.height)/10} |
 				<small>Weight(kg): </small>${(pokeman.weight)/10} |
-            	<small>Type: </small>${caps(type)}</p>
+				<small>Type: </small>${caps(type)}</p>
+				
         	</div>
 		</div>
 		`;
@@ -78,3 +79,51 @@ const closepopup = () =>{
 };
 
 fetchPokemon();
+
+function myFunction() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('myInput');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("pokedex");
+    li = ul.getElementsByClassName('card');
+  
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+      a = li[i].getElementsByTagName("a")[0];
+      txtValue = a.textContent || a.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
+
+  const searchBox = document.querySelector(".search-box");
+      const searchBtn = document.querySelector(".search-icon");
+      const cancelBtn = document.querySelector(".cancel-icon");
+      const searchInput = document.querySelector("input");
+      const searchData = document.querySelector(".search-data");
+      searchBtn.onclick =()=>{
+        searchBox.classList.add("active");
+        searchBtn.classList.add("active");
+        searchInput.classList.add("active");
+        cancelBtn.classList.add("active");
+        searchInput.focus();
+        if(searchInput.value != ""){
+          var values = searchInput.value;
+          searchData.classList.remove("active");
+          searchData.innerHTML = "You just typed " + "<span style='font-weight: 500;'>" + values + "</span>";
+        }else{
+          searchData.textContent = "";
+        }
+      }
+      cancelBtn.onclick =()=>{
+        searchBox.classList.remove("active");
+        searchBtn.classList.remove("active");
+        searchInput.classList.remove("active");
+        cancelBtn.classList.remove("active");
+        searchData.classList.toggle("active");
+        searchInput.value = "";
+      }
